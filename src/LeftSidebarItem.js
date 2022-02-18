847fbc6e18fc557fc0895da5c83f1a6f790596f0
IDO77777 <hayato_ido_za@mail.toyota.co.jp>
2022-02-18 15:10:26 +0900

    commit

--- src/LeftSidebarItem.js
+++ src/LeftSidebarItem.js
@@ -11,11 +11,33 @@ const images = [
 ]
 
 const shapes = [
-  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,  type: 'shape'},
-  {src: `data:image/svg+xml,%0A%3Csvg width='100' height='120' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M2197 1105h789v480h-789z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-2197 -1105)'%3E%3Cpath d='M2200.5 1187.33c0-43.53 35.3-78.83 78.83-78.83h624.34c43.53 0 78.83 35.3 78.83 78.83v315.34c0 43.53-35.3 78.83-78.83 78.83h-624.34c-43.53 0-78.83-35.3-78.83-78.83Z' stroke='%23000' stroke-width='6.875' stroke-miterlimit='8' fill='none' fill-rule='evenodd'/%3E%3C/g%3E%3C/svg%3E`,  type: 'shape'},
-  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 100'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M2326 471h766v127h-766z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-2326 -471)'%3E%3Cpath d='M2326.5 524.188h650.34v20.625H2326.5Zm640.03-20.625 61.87 30.937-61.87 30.938Z'/%3E%3C/g%3E%3C/svg%3E`,  type: 'shape'},
-  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 100'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M2263 688h766v127h-766z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-2263 -688)'%3E%3Cpath d='M3028.4 741.188h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-72.842v20.625h72.842Zm-62.53-20.625-61.875 30.937 61.875 30.938Z'/%3E%3C/g%3E%3C/svg%3E`,  type: 'shape'},
-  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 200'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M3237 366h829v232h-829z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-3237 -366)'%3E%3Cpath d='M3352.06 524.188h598.78v20.625h-598.78Zm10.31 41.25L3300.5 534.5l61.87-30.937Zm578.16-61.875 61.87 30.937-61.87 30.938Z'/%3E%3Cpath d='M3631.5 535c0-10.769 8.73-19.5 19.5-19.5s19.5 8.731 19.5 19.5c0 10.77-8.73 19.5-19.5 19.5s-19.5-8.73-19.5-19.5Z' stroke='%23000' stroke-width='4.583' stroke-miterlimit='8' fill-rule='evenodd'/%3E%3Ctext font-family='Arial,Arial_MSFontService,sans-serif' font-weight='400' font-size='83' transform='translate(3601.11 471)'%3E○○%3C/text%3E%3C/g%3E%3C/svg%3E`,  type: 'shape'},
+  //工程
+  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,
+   color: '#d3d3d3', width: '79', height: '500', type: '3', opacity: '1', classname: 'process', text: '工程'},
+  //工程 1/2
+  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,
+  color: '#d3d3d3', width: '79', height: '245', type: '3', opacity: '1', classname: 'process2', text: '工程1/2'},
+  //工程 1/3
+  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,
+   color: '#d3d3d3', width: '79', height: '160', type: '3', opacity: '1', classname: 'process3', text: '工程1/3'},
+  //工程 1/4
+  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,
+  color: '#d3d3d3', width: '79', height: '120', type: '3', opacity: '1', classname: 'process4', text: '工程1/4'},
+  //工程 1/16
+  {src: `data:image/svg+xml,%3Csvg width='50' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0,0,300,1800'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M1407 435h281v1729h-281z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-1407 -435)'%3E%3Cpath stroke='%23000' stroke-width='10.313' stroke-miterlimit='8' fill='%23D3D3D3' d='M1412 440h271v1719h-271z'/%3E%3C/g%3E%3C/svg%3E`,
+  color: '#d3d3d3', width: '79', height: '30', type: '3', opacity: '1', classname: 'process16', text: '工程1/16'},
+]
+
+const lines = [
+  //物の流れ
+  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 100'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M2326 471h766v127h-766z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-2326 -471)'%3E%3Cpath d='M2326.5 524.188h650.34v20.625H2326.5Zm640.03-20.625 61.87 30.937-61.87 30.938Z'/%3E%3C/g%3E%3C/svg%3E`,
+   start: '0', end: '8', style: '2', type: '', classname: 'obj-flow', text: '物の流れ'},
+  //情報の流れ
+  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 100'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M2263 688h766v127h-766z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-2263 -688)'%3E%3Cpath d='M3028.4 741.188h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-82.5v20.625h82.5Zm-144.375 0h-72.842v20.625h72.842Zm-62.53-20.625-61.875 30.937 61.875 30.938Z'/%3E%3C/g%3E%3C/svg%3E`,
+   start: '8', end: '0', style: '1', type: '', classname: 'info-flow', text: '情報の流れ'},
+  //リードタイム
+  {src: `data:image/svg+xml,%3Csvg width='300' height='100' xmlns='http://www.w3.org/2000/svg' overflow='hidden' viewBox='0, 0, 800, 200'%3E%3Cdefs%3E%3CclipPath id='a'%3E%3Cpath d='M3237 366h829v232h-829z'/%3E%3C/clipPath%3E%3C/defs%3E%3Cg clip-path='url(%23a)' transform='translate(-3237 -366)'%3E%3Cpath d='M3352.06 524.188h598.78v20.625h-598.78Zm10.31 41.25L3300.5 534.5l61.87-30.937Zm578.16-61.875 61.87 30.937-61.87 30.938Z'/%3E%3Cpath d='M3631.5 535c0-10.769 8.73-19.5 19.5-19.5s19.5 8.731 19.5 19.5c0 10.77-8.73 19.5-19.5 19.5s-19.5-8.73-19.5-19.5Z' stroke='%23000' stroke-width='4.583' stroke-miterlimit='8' fill-rule='evenodd'/%3E%3Ctext font-family='Arial,Arial_MSFontService,sans-serif' font-weight='400' font-size='83' transform='translate(3601.11 471)'%3E○○%3C/text%3E%3C/g%3E%3C/svg%3E`,
+   start: '8', end: '8', style: '2', type: 'LT', classname: 'obj-flow', text: 'リードタイム'},
 ]
 
 // 「物と情報の流れ図」記号の画像を取得する
@@ -30,56 +52,31 @@ function getImage(img) {
 
 // 「工程」「会社・組織」の記号をツールバーに描画する
 function addShapes(container) {
-  container.innerHTML += `<div class="shape draggable-item"
-                          background-color="#d3d3d3"
-                          shape-width=79
-                          shape-height=500
-                          shape-type=3
-                          shape-opacity=1
-                          data-image-url="${shapes[0].src}">
-                          <div class="item-frame"><div class="process"></div></div>
-                          <div class="item-text">工程</div>
-			                    </div>`
-                          // <div class="shape draggable-item"
-                          // background-color="#FFFFFF"
-                          // shape-width=380
-                          // shape-height=200
-                          // shape-type=7
-                          // shape-opacity=0
-                          // shape-name="organization"
-                          // data-image-url="${shapes[1].src}">
-                          // <div class="item-frame"><div class="organization"></div></div>
-                          // <div class="item-text">会社・組織</div>
-			                    // </div>
+  shapes.forEach(elem => 
+    container.innerHTML += `<div class="shape draggable-item"
+                            shape-image-url="${elem.src}"
+                            background-color="${elem.color}"
+                            shape-width=${elem.width}
+                            shape-height=${elem.height}
+                            shape-type=${elem.type}
+                            shape-opacity=${elem.opacity}>
+                            <div class="item-frame"><div class="${elem.classname}"></div></div>
+                            <div class="item-text">${elem.text}</div>
+                            </div>`);
 }
 
 // 「物の流れ」「情報の流れ」「リードタイム」の記号をツールバーに描画する
 function addLines(container) {
+  lines.forEach(elem => 
   container.innerHTML += `<div class="line draggable-item"
-                          line-start=0 
-                          line-end=8 
-                          line-style=2
-                          data-image-url="${shapes[2].src}">
-                          <div class="item-frame"><div class="obj-flow"></div></div>
-                          <div class="item-text">物の流れ</div>
-                          </div>
-                          <div class="line draggable-item"
-                          line-start=8
-                          line-end=0
-                          line-style=1
-                          data-image-url="${shapes[3].src}">
-                          <div class=" item-frame"><div class="info-flow"></div></div>
-                          <div class="item-text">情報の流れ</div>
-                          </div>
-                          <div class="line draggable-item"
-                          line-start=8 
-                          line-end=8 
-                          line-style=2
-                          line-type="LT"
-                          data-image-url="${shapes[4].src}">
-                          <div class=" item-frame"><div class="lead-time"></div></div>
-                          <div class="item-text">リードタイム</div>
-                          </div>`
+                          line-image-url="${elem.src}"
+                          line-start=${elem.start}
+                          line-end=${elem.end}
+                          line-style=${elem.style}
+                          line-type="${elem.type}">
+                          <div class="item-frame"><div class="${elem.classname}"></div></div>
+                          <div class="item-text">${elem.text}</div>
+                          </div>`);
 }
 
 // 取得した「物と情報の流れ図」記号の画像をツールバーに表示する
@@ -99,29 +96,54 @@ function createImage(canvasX, canvasY, url) {
 }
 
 // ボードに図形を描画する
-function createShape(canvasX, canvasY, wid, hei, color, text, stype, sopacity, sname) {
-  if(sname == "organization"){
+function createShape(canvasX, canvasY, shapewidget) {
+  if(shapewidget.name == "organization"){
     createText(canvasX - 30, canvasY + 150, "○○組織・会社", 3.42)
   }
   return miro.board.widgets.create({
     type: 'shape',
     x: canvasX,
     y: canvasY,
-    width: Number(wid),
-    height: Number(hei),
+    width: Number(shapewidget.width),
+    height: Number(shapewidget.height),
     style: {
       textColor: '#000',
-      backgroundColor: color,
-      backgroundOpacity: sopacity,
+      backgroundColor: shapewidget.color,
+      backgroundOpacity: shapewidget.opacity,
       borderWidth: 2,
       borderColor: '#000', //'transparent',
       fontFamily: 0,
       fontSize: 36,
       textColor: '#000',
-      shapeType: stype,
+      shapeType: shapewidget.type,
     },
   })
 }
+
+// // ボードに図形を描画する
+// function createShape(canvasX, canvasY, wid, hei, color, text, stype, sopacity, sname) {
+//   if(sname == "organization"){
+//     createText(canvasX - 30, canvasY + 150, "○○組織・会社", 3.42)
+//   }
+//   return miro.board.widgets.create({
+//     type: 'shape',
+//     x: canvasX,
+//     y: canvasY,
+//     width: Number(wid),
+//     height: Number(hei),
+//     style: {
+//       textColor: '#000',
+//       backgroundColor: color,
+//       backgroundOpacity: sopacity,
+//       borderWidth: 2,
+//       borderColor: '#000', //'transparent',
+//       fontFamily: 0,
+//       fontSize: 36,
+//       textColor: '#000',
+//       shapeType: stype,
+//     },
+//   })
+// }
 
 // ボードに線を描画する
 function createLine(canvasX, canvasY, sstyle, estyle, lstyle, linetype) {
@@ -213,34 +235,19 @@ function bootstrap() {
   }
   miro.board.ui.initDraggableItemsContainer(container, imageOptions)
 
-
-  let currentShapeColor
-  let currentShapeWidth
-  let currentShapeHeight
-  let currentShapeText
-  let currentShapeType
-  let currentShapeOpacity
-  let currentShapeName
   const shapeOptions = {
     draggableItemSelector: '.shape ',
     getDraggableItemPreview: (targetElement) => {
-      currentShapeColor = targetElement.getAttribute('background-color')
-      currentShapeWidth = targetElement.getAttribute('shape-width')
-      currentShapeHeight = targetElement.getAttribute('shape-height')
-      currentShapeText = targetElement.innerText
-      currentShapeType = targetElement.getAttribute('shape-type')
-      currentShapeOpacity = targetElement.getAttribute('shape-opacity')
-      currentShapeName = targetElement.getAttribute('shape-name')
-      currentImageUrl = targetElement.getAttribute('data-image-url')
+      shapeWidgetCreate(targetElement)
       return {
         width: 150,
         height: 150,
-        url: currentImageUrl,
+        url: shapewidget.url,
       }
     },
     onDrop: (canvasX, canvasY) => {
       console.log('onDrop 2')
-      createShape(canvasX, canvasY, currentShapeWidth, currentShapeHeight, currentShapeColor, currentShapeText, currentShapeType, currentShapeOpacity, currentShapeName)
+      createShape(canvasX, canvasY, shapewidget)
     },
   }
   miro.board.ui.initDraggableItemsContainer(container, shapeOptions)
@@ -257,7 +264,7 @@ function bootstrap() {
       currentlineEndStyle = targetElement.getAttribute('line-end')
       currentlineStyle = targetElement.getAttribute('line-style')
       currentlineType = targetElement.getAttribute('line-type')
-      currentImageUrl = targetElement.getAttribute('data-image-url')
+      currentImageUrl = targetElement.getAttribute('line-image-url')
       return {
         url: currentImageUrl
       }
@@ -271,3 +278,26 @@ function bootstrap() {
 }
 
 miro.onReady(bootstrap)
+
+
+let shapewidget = {
+  color:'',
+  width:'',
+  height:'',
+  text:'',
+  type:'',
+  opacity:'',
+  name:'',
+  url:'',
+}
+
+function shapeWidgetCreate(target){
+  shapewidget.color = target.getAttribute('background-color')
+  shapewidget.width = target.getAttribute('shape-width')
+  shapewidget.height = target.getAttribute('shape-height')
+  shapewidget.text = target.innerText
+  shapewidget.type = target.getAttribute('shape-type')
+  shapewidget.opacity = target.getAttribute('shape-opacity')
+  shapewidget.name = target.getAttribute('shape-name')
+  shapewidget.url = target.getAttribute('shape-image-url')
+}
