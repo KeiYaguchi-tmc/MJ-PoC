4f82a0712ff0da864a230d740bd8020e72108f74
IDO77777 <hayato_ido_za@mail.toyota.co.jp>
2022-06-08 11:23:59 +0900

    停滞マークの倍率変更

--- src/LeftSidebarItem.js
+++ src/LeftSidebarItem.js
@@ -75,10 +75,18 @@ function addContents(container) {
 
 // ボードに画像を描画する
 function createImage(canvasX, canvasY, url) {
+    console.log(url.indexOf("icon05_.svg"));
+    //2022/06/08対応
+    //icon05:停滞マーク 縮小対応
+    //その他画像 変更なし(scale=10)
+    let img_scale = 10;
+    if (url.indexOf("icon05_.svg") !== -1) {
+        img_scale = 4;
+    }
     return miro.board.widgets.create({
         type: 'image',
         url: url,
-        scale: 10,
+        scale: img_scale,
         x: canvasX,
         y: canvasY,
     })
@@ -149,7 +157,9 @@ function bootstrap() {
         },
         onDrop: async(canvasX, canvasY) => {
             console.log('image create')
+            //画像作成
             await createImage(canvasX, canvasY, currentImageUrl)
+            //ストアマークだった場合、外枠をつける
             if (currentImageUrl.indexOf("icon08_") !== -1) {
                 miro.board.widgets.create({
                     type: 'shape',
